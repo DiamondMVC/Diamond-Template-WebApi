572ede7336dfe9db39d949318bc7b36abd9c2dd4
Jacob Jensen <jj_1337@live.dk>
2017-11-01 20:56:09 +0100

    Update homecontroller.d

--- src/controllers/homecontroller.d
+++ src/controllers/homecontroller.d
@@ -10,21 +10,16 @@ import diamond.controllers;
 /// The home controller.
 final class HomeController : Controller!HomeController
 {
-  import vibe.d : HTTPServerRequest, HTTPServerResponse;
-  import diamond.http : Route;
-
   public:
   final:
   /**
   * Creates a new instance of the home controller.
   * Params:
-  *   request =  The request passed to the controller.
-  *   response = The response passed to the controller.
-  *   route =    The route passed to the controller.
+  *   client =  The client passed to the controller.
   */
-  this(HTTPServerRequest request, HTTPServerResponse response, Route route)
+  this(HttpClient client)
   {
-    super(request,response,route,this);
+    super(client);
   }
 
   /// Route: / | /home
